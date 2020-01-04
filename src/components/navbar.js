1e2ae1f605aaea472d802b4d905e3e2c6c7e9d05
osogrizz <lhtor@gmail.com>
2020-01-04 12:51:51 -0800

    Added emotion styled-components to navbar.js

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,41 +1,49 @@
 import React from "react"
 import { Link } from "gatsby"
 
+import styled from "@emotion/styled"
+
+const NavWrapper = styled("nav")`
+  bottom: 0;
+  left: 0;
+  margin: 0;
+  margin-bottom: 1.45rem;
+  width: 100%;
+  background: #fff;
+  border-bottom: 2px solid #555;
+  padding: 12px;
+  box-shadow: 0 0 12px #666;
+`
+
+const NavContainer = styled("div")`
+  display: flex;
+  justify-content: space-between;
+  margin: 0 auto;
+  max-width: 960px;
+  text-transform: uppercase;
+  font-size: 0.775rem;
+  font-weight: 700;
+  letter-spacing: 1px;
+
+  a {
+    :hover {
+      color: #999;
+    }
+  }
+`
+
 const Navbar = () => {
   return (
-    <div
-      style={{
-        bottom: 0,
-        left: 0,
-        width: "100%",
-        background: "#fff",
-        borderBottom: "2px solid #555",
-        padding: "12px",
-        margin: 0,
-        marginBottom: `1.45rem`,
-        boxShadow: "0 0 12px #666",
-      }}
-    >
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "space-around",
-          margin: "0 auto",
-          maxWidth: "960px",
-          textTransform: "uppercase",
-          fontSize: "0.775rem",
-          fontWeight: "700",
-          letterSpacing: "1px",
-        }}
-      >
+    <NavWrapper>
+      <NavContainer>
         <Link to="/">Home</Link>
         <Link>Solutions</Link>
         <Link>Specialties</Link>
         <Link>Services & Support</Link>
         <Link to="about">About</Link>
         <Link>Contact Us</Link>
-      </div>
-    </div>
+      </NavContainer>
+    </NavWrapper>
   )
 }
 
