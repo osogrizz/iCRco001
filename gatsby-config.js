b189e086ef965be59f0b8b54b21da36abd0bdb9e
osogrizz <lhtor@gmail.com>
2020-01-05 01:38:25 -0800

    Fixes changed file name in gatsby-config.

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,7 +24,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/iCRco-logo.webp`, // This path is relative to the root of the site.
+        icon: `src/images/iCRco-logo.png`, // This path is relative to the root of the site.
       },
     },
     {
