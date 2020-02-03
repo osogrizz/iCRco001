1ea013188875f30a8fd8dd02408f853f919e377e
leotorres75 <ltorres@icrcompany.com>
2020-02-03 12:30:50 -0800

    Fixes typo for playsinline attribute.

--- src/pages/index.js
+++ src/pages/index.js
@@ -138,10 +138,10 @@ const IndexPage = ({ data }) => {
             loop
             muted
             autoPlay
-            playsInline
-          >
-            <source src={ShortVideo} type="video/mp4" />
-          </video>
+            playsinline
+            src={ShortVideo}
+            type="video/mp4"
+          ></video>
         </section>
 
         <SideNav>
