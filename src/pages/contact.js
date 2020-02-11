d58588eccc596ebb5bf0dc52e0acbf048d6abbc6
leotorres75 <ltorres@icrcompany.com>
2020-02-11 14:40:01 -0800

    Adds initial toggle contact form.

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,11 +1,35 @@
-import React from "react"
+import React, { useState } from "react"
 import styled from "@emotion/styled"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const FormContainer = styled("div")`
-  max-width: 960px;
+const ContactWrapper = styled("div")`
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  margin-top: 80px;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+
+  button {
+    margin: 20px 20px 0;
+    background: transparent;
+    border: none;
+    cursor: pointer;
+    outline: transparent;
+  }
+
+  h2 {
+    margin-top: 40px;
+    text-align: center;
+  }
+`
+
+const InputContainer = styled("div")`
+  max-width: 600px;
   margin: 0 auto;
   display: flex;
   flex-direction: column;
@@ -39,38 +63,105 @@ const FormContainer = styled("div")`
 `
 
 const Contact = () => {
+  const [form, setForm] = useState(false)
+  const [quote, setQuote] = useState(false)
+
+  const handleForm = e => {
+    setQuote(false)
+    setForm(!form)
+    console.log("form", form)
+  }
+  const handleQuote = e => {
+    setForm(false)
+    setQuote(!quote)
+    console.log("quote", quote)
+  }
+
   return (
     <Layout>
       <SEO title="contact" />
-      <div>
-        <form>
-          <FormContainer>
-            <div>
-              <input type="text" name="fName" placeholder="First Name" />
-              <input type="text" name="lName" placeholder="Last Name" />
-            </div>
-            <div>
-              <input type="email" name="email" placeholder="Email" />
-              <input type="tel" name="phone" placeholder="Phone" />
-            </div>
-            <div>
-              <input type="text" name="company" placeholder="Company" />
-            </div>
-            <div>
-              <textarea
-                name="questions"
-                id=""
-                cols="54"
-                rows="10"
-                placeholder="Questions"
-              ></textarea>
-            </div>
-            <div>
-              <button>Send</button>
-            </div>
-          </FormContainer>
-        </form>
-      </div>
+      <ContactWrapper>
+        <div>
+          <div>
+            <button onClick={handleForm} name="form">
+              General inquiry
+            </button>{" "}
+            |
+            <button onClick={handleQuote} name="quote">
+              Request a quote
+            </button>
+          </div>
+          <div style={{ marginTop: "40px" }}>
+            <hr />
+          </div>
+        </div>
+
+        {form ? (
+          <div>
+            <h2>General Questions</h2>
+            <form>
+              <InputContainer>
+                <div>
+                  <input type="text" name="fName" placeholder="First Name" />
+                  <input type="text" name="lName" placeholder="Last Name" />
+                </div>
+                <div>
+                  <input type="email" name="email" placeholder="Email" />
+                  <input type="tel" name="phone" placeholder="Phone" />
+                </div>
+                <div>
+                  <input type="text" name="company" placeholder="Company" />
+                </div>
+                <div>
+                  <textarea
+                    name="questions"
+                    id=""
+                    cols="54"
+                    rows="10"
+                    placeholder="Questions"
+                  ></textarea>
+                </div>
+                <div>
+                  <button onClick={e => e.preventDefault()}>Send</button>
+                </div>
+              </InputContainer>
+            </form>
+          </div>
+        ) : null}
+
+        {quote && (
+          <div>
+            <h2>Quote</h2>
+            <form>
+              <InputContainer>
+                <div>
+                  <input type="text" name="fName" placeholder="First Name" />
+                  <input type="text" name="lName" placeholder="Last Name" />
+                </div>
+                <div>
+                  <input type="email" name="email" placeholder="Email" />
+                  <input type="tel" name="phone" placeholder="Phone" />
+                </div>
+                <div>
+                  {/* <input type="text" name="company" placeholder="Company" /> */}
+                </div>
+                <div>
+                  <textarea
+                    name="questions"
+                    id=""
+                    cols="54"
+                    rows="10"
+                    placeholder="Questions"
+                  ></textarea>
+                </div>
+                <div>
+                  <button onClick={e => e.preventDefault()}>Send</button>
+                </div>
+              </InputContainer>
+            </form>
+          </div>
+        )}
+      </ContactWrapper>
     </Layout>
   )
 }
