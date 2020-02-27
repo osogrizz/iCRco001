e3b8e3fb8e4beb6627129e12df775164b9583e5d
leotorres75 <ltorres@icrcompany.com>
2020-02-27 14:45:31 -0800

    Removes unused files.

--- src/components/HeroImages/nurse-hero-3.js
+++ src/components/HeroImages/nurse-hero-3.js
@@ -6,29 +6,72 @@ import BackgroundImage from "gatsby-background-image"
 
 const ImageBackground = styled(BackgroundImage)`
   display: flex;
-  justify-content: center;
+  flex-direction: column;
+  /* justify-content: center; */
   align-items: center;
-  /* height: 100vh; */
+  height: 100vh;
   width: 100%;
   background-image: url("../images/AirDRLE_Nurse.png");
   background-size: cover cover;
-  margin: 0;
+  margin: 0 !important;
+  padding: 0 !important;
+`
+
+const Container = styled("div")`
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  display: flex;
+  width: 100%;
+  height: 95vh;
+  text-align: center;
+  flex-direction: column;
+  justify-content: space-evenly;
+  overflow: hidden;
   padding: 0;
+  margin: 0;
+
+  div {
+    width: 100%;
+    margin: 0 auto;
+    padding: 0;
+  }
+`
+const LowerContainer = styled("div")`
+  top: -50;
+  display: flex;
+  margin: 0 auto;
+  width: 800px !important;
+`
+
+const InfoContainer = styled("div")`
+  padding-bottom: 0;
+  margin-bottom: 0;
+  width: 120%;
+
+  hr {
+    height: 2px;
+    width: 580px;
+    margin: 0 auto 5px;
+    background: #333;
+  }
+`
+
+const InfoBox = styled("div")`
+  width: 200px;
+
+  p {
+    font-weight: 700;
+    letter-spacing: 2px;
+    margin: 0;
+  }
 `
 
 const NurseHero3 = () => {
-  const { image, measureC } = useStaticQuery(graphql`
+  const { image } = useStaticQuery(graphql`
     query {
       image: file(relativePath: { eq: "AirDRLE_Nurse.png" }) {
         sharp: childImageSharp {
           fluid(maxWidth: 1120, quality: 100) {
-            ...GatsbyImageSharpFluid
-          }
-        }
-      }
-      measureC: file(relativePath: { eq: "AirDRc_dimensions.png" }) {
-        sharp: childImageSharp {
-          fluid(maxWidth: 1120) {
             ...GatsbyImageSharpFluid
           }
         }
@@ -41,7 +84,31 @@ const NurseHero3 = () => {
       fluid={image.sharp.fluid}
       fadeIn="soft"
       preserveStackingContext
-    ></ImageBackground>
+    >
+      <Container>
+        <div>
+          <NurseHero3 />
+        </div>
+        <InfoContainer>
+          <LowerContainer>
+            <InfoBox style={{ borderRight: "2px solid black" }}>
+              <h2>100</h2>
+              <p>Microns</p>
+            </InfoBox>
+
+            <InfoBox style={{ margin: "0 20px" }}>
+              <h2>16</h2>
+              <p>Bit Resolutions</p>
+            </InfoBox>
+
+            <InfoBox style={{ borderLeft: "2px solid black" }}>
+              <h2>15</h2>
+              <p>Megapixel</p>
+            </InfoBox>
+          </LowerContainer>
+        </InfoContainer>
+      </Container>
+    </ImageBackground>
   )
 }
 
