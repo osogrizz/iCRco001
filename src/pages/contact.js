e9d3347087d4214f8441dc35f0ad6af7e439144d
osogrizz <lhtor@gmail.com>
2020-04-13 10:36:24 -0700

    Adds contact page styles

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -7,24 +7,41 @@ import SEO from "../components/seo"
 const ContactWrapper = styled("div")`
   font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
     Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
-  margin-top: 80px;
+  margin-top: 40px;
   width: 100%;
-  height: 100vh;
+  min-height: 100vh;
   display: flex;
   align-items: center;
   flex-direction: column;
 
-  button {
+  /* button {
     margin: 20px 20px 0;
     background: transparent;
     border: none;
     cursor: pointer;
     outline: transparent;
-  }
+  } */
 
   h2 {
-    margin-top: 40px;
+    margin-top: 20px;
     text-align: center;
+  }
+  button {
+    width: 200px;
+    margin: 40px 40px 0;
+    padding: 10px 20px;
+    background: #0069aa;
+    color: #fff;
+    border: none;
+    outline: orange;
+    cursor: pointer;
+    border-radius: 5px;
+  }
+
+  form {
+    background: #f4f5f5;
+    padding: 20px 60px;
+    border-radius: 8px;
   }
 `
 
@@ -34,31 +51,52 @@ const InputContainer = styled("div")`
   display: flex;
   flex-direction: column;
   justify-content: space-around;
-  padding-top: 40px;
   font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
     Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
-  margin-top: 80px;
 
   input {
     width: 250px;
     margin: 10px 10px;
     padding: 10px;
     outline-color: orange;
+    border: 1px solid #888;
   }
   textarea {
     padding: 10px;
     outline-color: orange;
+    max-width: 100%;
+    margin: 15px 10px 0;
   }
-  button {
-    width: 200px;
-    margin: 40px;
-    padding: 10px 20px;
-    background: #0069aa;
-    color: #fff;
-    border: none;
-    outline: orange;
-    cursor: pointer;
-    border-radius: 5px;
+
+  select {
+    width: 250px;
+    height: 45px;
+    margin: 10px 10px;
+    padding: 10px;
+    outline-color: orange;
+    border: 1px solid #888;
+  }
+
+  .btn-container {
+    display: flex;
+    justify-content: center;
+
+    button {
+      width: 200px;
+      margin: 40px auto;
+      padding: 10px 20px;
+      background: #0069aa;
+      color: #fff;
+      border: none;
+      outline: orange;
+      cursor: pointer;
+      border-radius: 5px;
+    }
+  }
+
+  p {
+    font-size: 0.95rem;
+    font-style: italic;
   }
 `
 
@@ -77,6 +115,10 @@ const Contact = () => {
     console.log("quote", quote)
   }
 
+  const handleSubmit = e => {
+    // e.preventDefault()
+  }
+
   return (
     <Layout>
       <SEO title="contact" />
@@ -84,22 +126,79 @@ const Contact = () => {
         <div>
           <div>
             <button onClick={handleForm} name="form">
-              General inquiry
-            </button>{" "}
-            |
+              General Inquiry
+            </button>
             <button onClick={handleQuote} name="quote">
-              Request a quote
+              Request a Quote
             </button>
           </div>
-          <div style={{ marginTop: "40px" }}>
+          <div style={{ marginTop: "25px" }}>
             <hr />
           </div>
         </div>
 
         {form ? (
           <div>
-            <h2>General Questions</h2>
             <form>
+              <h2>General Questions</h2>
+              <InputContainer>
+                <div>
+                  <input
+                    type="text"
+                    name="fName"
+                    placeholder="First Name"
+                    required
+                  />
+                  <input
+                    type="text"
+                    name="lName"
+                    placeholder="Last Name"
+                    required
+                  />
+                </div>
+                <div>
+                  <input
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                    required
+                  />
+                  <input type="tel" name="phone" placeholder="Phone" required />
+                </div>
+                <div>
+                  <input
+                    type="text"
+                    name="company"
+                    placeholder="Company"
+                    required
+                  />
+                </div>
+                <div>
+                  <textarea
+                    name="questions"
+                    id=""
+                    cols="54"
+                    rows="10"
+                    placeholder="Questions"
+                    required
+                  ></textarea>
+                </div>
+                <div className="btn-container">
+                  <button onClick={handleSubmit}>Send</button>
+                </div>
+                <p>
+                  *We take your privacy seriously and will not sell your
+                  information to anyone.
+                </p>
+              </InputContainer>
+            </form>
+          </div>
+        ) : null}
+
+        {quote && (
+          <div>
+            <form>
+              <h2>Request a Quote</h2>
               <InputContainer>
                 <div>
                   <input type="text" name="fName" placeholder="First Name" />
@@ -111,40 +210,30 @@ const Contact = () => {
                 </div>
                 <div>
                   <input type="text" name="company" placeholder="Company" />
-                </div>
-                <div>
-                  <textarea
-                    name="questions"
-                    id=""
-                    cols="54"
-                    rows="10"
-                    placeholder="Questions"
-                  ></textarea>
-                </div>
-                <div>
-                  <button onClick={e => e.preventDefault()}>Send</button>
-                </div>
-              </InputContainer>
-            </form>
-          </div>
-        ) : null}
 
-        {quote && (
-          <div>
-            <h2>Quote</h2>
-            <form>
-              <InputContainer>
-                <div>
-                  <input type="text" name="fName" placeholder="First Name" />
-                  <input type="text" name="lName" placeholder="Last Name" />
+                  <select name="" id="">
+                    <option value="">Choose your product</option>
+                    <option value="">AirDR</option>
+                    <option value="">AirDR c</option>
+                    <option value="">AirDR LE</option>
+                    <option value="">AirDR LE c</option>
+                    <option value="">{`AirDR-P (Cesium)`}</option>
+                    <option value="">{`AirDR-P c (Cesium)`}</option>
+                    <option value="">{`AirDR-P (Gadox)`}</option>
+                    <option value="">{`AirDR-P c (Gadox)`}</option>
+                    <option value="">AirDR-M</option>
+                    <option value="">iCR 3600LF</option>
+                    <option value="">iCR Chrome</option>
+                    <option value="">iCR Chrome M</option>
+                    <option value="">Claris XT</option>
+                    <option value="">Claris V</option>
+                    <option value="">Crystal Imager</option>
+                    <option value="">Crystal-M Imager</option>
+                    <option value="">Clarity PACS</option>
+                    <option value="">XC</option>
+                  </select>
                 </div>
-                <div>
-                  <input type="email" name="email" placeholder="Email" />
-                  <input type="tel" name="phone" placeholder="Phone" />
-                </div>
-                <div>
-                  {/* <input type="text" name="company" placeholder="Company" /> */}
-                </div>
+
                 <div>
                   <textarea
                     name="questions"
@@ -154,9 +243,13 @@ const Contact = () => {
                     placeholder="Questions"
                   ></textarea>
                 </div>
-                <div>
-                  <button onClick={e => e.preventDefault()}>Send</button>
+                <div className="btn-container">
+                  <button onClick={handleSubmit}>Send</button>
                 </div>
+                <p>
+                  *We take your privacy seriously and will not sell your
+                  information to anyone.
+                </p>
               </InputContainer>
             </form>
           </div>
